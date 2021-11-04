9c219fa57d347e83ef9493042aa6409e79337a6d
Valery Efremov <valeriy131100@gmail.com>
2021-11-04 15:41:32 +0300

    Extract link shortening code into function

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ import requests
 with open('token.txt', 'r') as token_file:
     token = token_file.readline()
 
-headers = {
-    'Authorization': f'Bearer {token}',
-    'Content-Type': 'application/json'
-}
 
-shortener_url = 'https://api-ssl.bitly.com/v4/shorten'
+def shorten_link(token, url):
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
+    shortener_url = 'https://api-ssl.bitly.com/v4/shorten'
+    json = {
+        'long_url': url
+    }
+    response = requests.post(shortener_url, json=json, headers=headers)
+    if response.ok:
+        return response.json()['id']
+    else:
+        return None
+
+
 link_to_shorten = input('Input link to short: ')
-
-json = {
-    'long_url': link_to_shorten
-}
-
-response = requests.post(shortener_url, json=json, headers=headers)
-
-if response.ok:
-    print(response.json()['id'])
-else:
-    print(response.status_code)
+print('Битлинк', shorten_link(token, link_to_shorten))
