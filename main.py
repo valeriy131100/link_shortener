de0984542f9c08626ba3d94992e0bb005d7b71a8
Valery Efremov <valeriy131100@gmail.com>
2021-11-04 15:21:30 +0300

    Add link shortening

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+import requests
+
 with open('token.txt', 'r') as token_file:
     token = token_file.readline()
+
+headers = {
+    'Authorization': f'Bearer {token}',
+    'Content-Type': 'application/json'
+}
+
+shortener_url = 'https://api-ssl.bitly.com/v4/shorten'
+link_to_shorten = input('Input link to short: ')
+
+json = {
+    'long_url': link_to_shorten
+}
+
+response = requests.post(shortener_url, json=json, headers=headers)
+
+if response.ok:
+    print(response.json()['id'])
+else:
+    print(response.status_code)
