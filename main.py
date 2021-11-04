e8721a61c3978905c4202ebd812c448e1022be4f
Valery Efremov <valeriy131100@gmail.com>
2021-11-04 16:06:49 +0300

    Add count_clicks function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from urllib.parse import urlparse
 
 with open('token.txt', 'r') as token_file:
     token = token_file.readline()
@@ -17,6 +18,21 @@ def shorten_link(token, url):
 
     response.raise_for_status()
     return response.json()['id']
+
+
+def count_clicks(token, link):
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
+    parsed_link = urlparse(link)
+    cleared_link = parsed_link.netloc + parsed_link.path
+
+    counter_url = f'https://api-ssl.bitly.com/v4/bitlinks/{cleared_link}/clicks/summary'
+    response = requests.get(counter_url, headers=headers)
+
+    response.raise_for_status()
+    return response.json()['total_clicks']
 
 
 link_to_shorten = input('Input link to short: ')
